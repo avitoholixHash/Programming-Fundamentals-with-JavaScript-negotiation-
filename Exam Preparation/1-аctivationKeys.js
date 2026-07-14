83b5d42b747652e04e9ea45724ef605a0205908d
Dobri Dashev <114178116+avitoholixHash@users.noreply.github.com>
2026-07-14 23:11:19 +0300

    1,5

--- Exam Preparation/1-аctivationKeys.js
+++ Exam Preparation/1-аctivationKeys.js
@@ -1,55 +1,55 @@
 function activationKeys(input) {
-    let activationKey = input.shift().split('');
+    let key = input.shift();
+    let instructionLine = input.shift().split('>>>');
 
-    while (input[0] !== "Generate") {
-        let line = input.shift();
-        let commandLine = line.split('>>>');
-        let subCommand = '';
-        let startIndex = 0;
-        let endIndex = 0;
 
-        switch (commandLine[0]) {
-            case 'Slice': 
-                startIndex = Number(commandLine[1]);
-                endIndex = Number(commandLine[2]);
-                activationKey.splice(startIndex, endIndex - startIndex);
-                console.log(activationKey.join(''));
+    while (instructionLine[0] !== "Generate") {
+        const command = instructionLine.shift();
+
+        switch (command) {
+            case 'Slice': {
+                const startIndex = Number(instructionLine[0]);
+                const endIndex = Number(instructionLine[1]);
+                // const substring = key.substring(startIndex, endIndex);
+                // key = key.replace(substring, '');
+                key = key.slice(0, startIndex) + key.slice(endIndex);
+                console.log(key);
+            }
                 break;
-            case 'Flip':
-                subCommand = commandLine[1];
-                startIndex = Number(commandLine[2]);
-                endIndex = Number(commandLine[3])
-                if (subCommand === 'Upper') {
-                    let upperCase = activationKey.slice(startIndex, endIndex).join('').toUpperCase();
-                    activationKey.splice(startIndex, endIndex - startIndex, ...upperCase);
-                    console.log(activationKey.join(''));
 
-                } else if (subCommand === 'Lower') {
-                    let lowerCase = activationKey.slice(startIndex, endIndex).join('').toLowerCase();
-                    activationKey.splice(startIndex, endIndex - startIndex, ...lowerCase);
-                    console.log(activationKey.join(''));
-                }
+            case 'Flip': {
+                const casing = instructionLine[0];
+                const startIndex = Number(instructionLine[1]);
+                const endIndex = Number(instructionLine[2]);
+                const substring = key.substring(startIndex, endIndex);
+                const newSubstring = casing == 'Upper' ? substring.toUpperCase() : substring.toLowerCase();
+
+                key = key.replace(substring, newSubstring);
+                console.log(key);
+            }
                 break;
-            case 'Contains':
-                subCommand = commandLine[1];
-                if (activationKey.includes(subCommand)) {
-                    console.log(`${activationKey.join('')} contains ${subCommand}`);
+
+            case 'Contains': {
+                const substring = instructionLine[0];
+
+                if (key.includes(substring)) {
+                    console.log(`${key} contains ${substring}`);
                 } else {
                     console.log('Substring not found!');
-
                 }
+            }
+                break;
         }
+        instructionLine = input.shift().split('>>>');
+    }
 
-    }
-    if (input[0] === 'Generate') {
-        console.log(`Your activation key is: ${activationKey.join('')}`);
-    }
+    console.log(`Your activation key is: ${key}`);
 }
 activationKeys((["134softsf5ftuni2020rockz42",
     "Slice>>>3>>>7",
-    "Contains>>>-rock",
-    "Contains>>>-uni-",
-    "Contains>>>-rocks",
+    "Contains>>>rock",
+    "Contains>>>uni-",
+    "Contains>>>rocks",
     "Flip>>>Upper>>>2>>>8",
     "Flip>>>Lower>>>5>>>11",
     "Generate"]));
